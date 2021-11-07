a79b9bad3bd6023762e3fd57e86d6c2650e8e0d0
Kirill Sokirka <sokirka2003@gmail.com>
2021-11-07 21:58:04 +0200

    HUETAAAAAAAAAAAAA

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@ from config import db
 
 
 class User(db.Model):
+    __tablename__ = "usersss"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), unique=False, nullable=True)
     email = db.Column(db.String(200), unique=False, nullable=True)
