d6799f8484fa4e68b21a9e3fdf53f849597c5113
Kirill Sokirka <sokirka2003@gmail.com>
2021-11-08 23:37:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 HelloSticker = 'stickers/HelloUser.tgs'
 UnknownText = 'stickers/UnknownText.tgs'
 BOT_TOKEN = '2089991556:AAFb0igp6cEFMoKKTq7Wdcg9JIDTPEExzDU'
-APP_URL = f'https://tickteeeeeeeer.herokuapp.com/{BOT_TOKEN}'
+APP_URL = f'https://ticketeeeeeeeeeeeeeeeeer.herokuapp.com/{BOT_TOKEN}'
 LOCAL_DB = 'postgresql+psycopg2://postgres:aa6400vt@localhost:5432/ticketsdb'
 HEROKU_DB = os.getenv('DATABASE_URL')
 
