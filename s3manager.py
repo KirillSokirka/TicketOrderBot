20ae4b8bea28001beefefcd70d4fa9a9a4fc461a
Kirill Sokirka <sokirka2003@gmail.com>
2021-11-09 20:00:28 +0200

    Update requirements

--- s3manager.py
+++ s3manager.py
@@ -5,8 +5,6 @@ import os
 session = boto3.Session(
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-
-
 s3 = session.resource('s3')
 
 
